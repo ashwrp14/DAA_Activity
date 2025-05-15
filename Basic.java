7d4c9761d242805ac9924e57dba1e3292da0b2c4
ASHWINI R P <ashwrp14@gmail.com>
2025-05-15 19:06:16 +0530

    Basic.java

---
+++ Basic.java
@@ -0,0 +1,7 @@
+public class Basic {
+
+    public static void main(String[] args) {
+       System.out.println("Hello, World.");
+System.out.println("Hello"); /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
