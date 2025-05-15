556f70e9a47f2381c19d9aa40b39d196c76f88b1
ASHWINI R P <ashwrp14@gmail.com>
2025-05-15 18:48:15 +0530

    Output.java
    String Output Formating

---
+++ Output.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Output {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                int len= s1.length();
+                //Complete this line
+                            System.out.print(s1);
+            int x_length = String.valueOf(x).length();
+            for(int j=0;j<15-len;j++)
+            {
+                System.out.print(" ");
+            }
+            if(x_length==2)
+            {
+                System.out.println("0"+x);
+            }
+            else if(x_length ==1)
+            {
+               System.out.println("00"+x); 
+            }
+            else
+            {
+                System.out.println(x); 
+            }
+
+            }
+            System.out.println("================================");
+
+    }
+}
+
+
+
